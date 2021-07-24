b9f7e1a4871d8f7e00b851935dc6a9deb7b7d685
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-24 13:10:44 -0300

    Trabalhando com SortedSet

--- TrabalhandoComConjuntos/Program.cs
+++ TrabalhandoComConjuntos/Program.cs
@@ -6,36 +6,11 @@ using System.Threading.Tasks;
 
 namespace TrabalhandoComConjuntos
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            HashSet<string> devedores = new HashSet<string>();
-
-            devedores.Add("Safira");
-            devedores.Add("Bob");
-
-            int elementos = devedores.Count();
-
-            Console.WriteLine(elementos);
-
-            //Não é possível adicionar elementos repetidos
-
-            devedores.Add("Safira");
-
-            elementos = devedores.Count();
-
-            bool contem = devedores.Contains("Safira");
-
-            Console.WriteLine(contem);
-
-            //Não é possível acessar a posição do conjuntos. pois seus elementos não possuem ordenação bem determinada
-            //devedores[0];
-
-            foreach (var devedor in devedores)
-            {
-                Console.WriteLine(devedor);
-            }
+            TesteSortedSet();
 
             Console.ReadLine();
 
--- TrabalhandoComConjuntos/TrabalhandoComSortedSet.cs
+++ TrabalhandoComConjuntos/TrabalhandoComSortedSet.cs
@@ -2,42 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using System.Threading.Tasks;
 
 namespace TrabalhandoComConjuntos
 {
     partial class Program
     {
-        static void TrablhandoComHashSet()
+        static void TesteSortedSet()
         {
-            HashSet<string> devedores = new HashSet<string>();
+            SortedSet<string> devedores = new SortedSet<string>();
 
             devedores.Add("Safira");
-            devedores.Add("Bob");
+            devedores.Add("BOB");
+            devedores.Add("Stuart");
+            devedores.Add("Stevan");
+            devedores.Add("Fabricio");
+            devedores.Add("Fernando");
+            devedores.Add("Edward");
 
             int elementos = devedores.Count();
+            Console.WriteLine(elementos);
+
+            //Também não é permitido adicionar itens repetidos
+            devedores.Add("Safira");
+            elementos = devedores.Count();
 
             Console.WriteLine(elementos);
 
-            //Não é possível adicionar elementos repetidos
-
-            devedores.Add("Safira");
-
-            elementos = devedores.Count();
-
-            bool contem = devedores.Contains("Safira");
-
-            Console.WriteLine(contem);
-
-            //Não é possível acessar a posição do conjuntos. pois seus elementos não possuem ordenação bem determinada
-            //devedores[0];
-
+            //Os elementos são guardados em ordem crescente
             foreach (var devedor in devedores)
             {
                 Console.WriteLine(devedor);
             }
         }
     }
-
 }
