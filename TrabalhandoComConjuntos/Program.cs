76f0ee0ce5b1f9f99b1437be129f780c55b0a88c
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-24 12:53:07 -0300

    Trabalhando com HashSet

---
+++ TrabalhandoComConjuntos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> devedores = new HashSet<string>();
+
+            devedores.Add("Safira");
+            devedores.Add("Bob");
+
+            int elementos = devedores.Count();
+
+            Console.WriteLine(elementos);
+
+            //Não é possível adicionar elementos repetidos
+
+            devedores.Add("Safira");
+
+            elementos = devedores.Count();
+
+            bool contem = devedores.Contains("Safira");
+
+            Console.WriteLine(contem);
+
+            //Não é possível acessar a posição do conjuntos. pois seus elementos não possuem ordenação bem determinada
+            //devedores[0];
+
+            foreach (var devedor in devedores)
+            {
+                Console.WriteLine(devedor);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
